52491051f003b4860955e32eec557ca9cfdadb31
qiu <18915410342@126.com>
2015-05-10 13:19:34 +0800

    Initial Commit

---
+++ CustomerApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomerApp
+//
+//  Created by 邱成西 on 15/5/10.
+//  Copyright (c) 2015年 邱成西. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
